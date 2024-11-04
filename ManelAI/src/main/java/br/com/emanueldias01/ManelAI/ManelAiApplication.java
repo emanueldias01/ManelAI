2bba728339d44182de67bab56d00dac5493734d9
emanueldias01 <emanueldias01dev@gmail.com>
2024-11-04 13:34:08 -0300

    upload files

---
+++ ManelAI/src/main/java/br/com/emanueldias01/ManelAI/ManelAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.ManelAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManelAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManelAiApplication.class, args);
+	}
+
+}
---
+++ ManelAI/src/test/java/br/com/emanueldias01/ManelAI/ManelAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.ManelAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManelAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
